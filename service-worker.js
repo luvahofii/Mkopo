8d67b0133224fadb77c62f63ab03464ffabce6a1
luvahofii <110642452+luvahofii@users.noreply.github.com>
2025-04-29 16:50:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,11 @@
-const CACHE_NAME = "mkopo-chapchap-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/logo.png",
-  "/favicon.png",
-  "/airtel.png",
-  "/vodacom.png",
-  "/yas.png",
-  "/halotel.png",
-  "/nmb.png",
-  "/crdb.png",
-  "/azania.png",
-  "/exim.png",
-  "/nbc.png",
-  "/icon-192.png",
-  "/icon-512.png"
-  // Add other critical files like CSS, JS, HTML
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', event => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
